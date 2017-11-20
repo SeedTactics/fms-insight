c6242b9b1d15751390a13d6e3d2e498113c5b9f6
John Lenz <wuzzeb@gmail.com>
2017-11-19 22:33:33 -0600

    Add appveyor and build script

---
+++ build.py
@@ -0,0 +1,68 @@
+import subprocess
+import re
+
+def freplace(filename, match, new):
+    with open(filename) as f:
+        ct = f.read()
+    ct = re.sub(match, new, ct)
+    with open(filename, "w") as f:
+        f.write(ct)
+
+def run(cmd, d):
+    print(d + "$ " + cmd)
+    subprocess.run(args=cmd.split(" "), cwd=d, check=True)
+
+# Check if current rev is a tag
+curtag = subprocess.check_output(["hg", "id", "-t", "-r", ".^"]).decode("utf-8")
+
+# Build mwi
+if curtag.startswith("mwi-"):
+    ver = curtag.replace("mwi-", "")
+    run("dotnet pack -c Release --include-symbols /p:VersionPrefix=" + ver,
+        "lib/BlackMaple.MachineWatchInterface")
+else:
+    run("dotnet build", "lib/BlackMaple.MachineWatchInterface")
+
+# Build framework
+if curtag.startswith("framework-"):
+    # Switch reference to use nuget seedorders
+    mwiver = subprocess.check_output(["hg", "id", "-t", "-r", "ancestors(.) and tag('re:mwi')"]).decode("utf-8")
+    mwiver = mwiver.replace("mwi-", "").split(".")[0]
+    freplace("lib/BlackMaple.MachineFramework/BlackMaple.MachineFramework.csproj",
+            r'<ProjectReference Include="../BlackMaple.MachineWatchInterface[^>]+>',
+             '<PackageReference Include="BlackMaple.MachineWatchInterface" Version="' + mwiver + '.*"/>')
+    freplace("test/test.csproj",
+            r'<ProjectReference Include="../lib/BlackMaple.MachineWatchInterface[^>]+>',
+             '<PackageReference Include="BlackMaple.MachineWatchInterface" Version="' + mwiver + '.*"/>')
+    
+    # Pack framework
+    ver = curtag.replace("framework-", "")
+    run("dotnet pack -c Release --include-symbols /p:VersionPrefix=" + ver,
+        "lib/BlackMaple.MachineFramework")
+else:
+    run("dotnet build", "lib/BlackMaple.MachineFramework")
+
+# Build service
+if curtag.startswith("service-"):
+    # Switch reference to use nuget mwi and framework
+    mwiver = subprocess.check_output(["hg", "id", "-t", "-r", "ancestors(.) and tag('re:mwi')"]).decode("utf-8")
+    mwiver = mwiver.replace("mwi-", "").split(".")[0]
+    freplace("src/service/MachineWatch.csproj",
+            r'<ProjectReference Include="../../lib/BlackMaple.MachineWatchInterface[^>]+>',
+             '<PackageReference Include="BlackMaple.MachineWatchInterface" Version="' + mwiver + '.*"/>')
+
+    frameworkver = subprocess.check_output(["hg", "id", "-t", "-r", "ancestors(.) and tag('re:framework')"]).decode("utf-8")
+    frameworkver = mwiver.replace("framework-", "").split(".")[0]
+    freplace("src/service/MachineWatch.csproj",
+            r'<ProjectReference Include="../../lib/BlackMaple.MachineFramework[^>]+>',
+             '<PackageReference Include="BlackMaple.MachineFramework" Version="' + frameworkver + '.*"/>')
+
+    # Pack service
+    ver = curtag.replace("service-", "")
+    run("dotnet pack -c Release --include-symbols /p:VersionPrefix=" + ver,
+        "src/service")
+else:
+    run("dotnet build", "src/service")
+
+# Build tests
+run("dotnet build", "test")
