a620c6e1968855b49de6fddd8826a2872b5577df
John Lenz <wuzzeb@gmail.com>
2018-02-05 21:51:12 -0600

    Add enzyme for testing

--- client/insight/src/App.test.tsx
+++ client/insight/src/App.test.tsx
@@ -1,14 +1,24 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import App from './App';
-import store from './data/store';
+// import { MemoryRouter } from 'react-router';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-    div);
+import App from './App';
+
+jest.mock('./data/store', () => {
+  return {
+    default: {
+      dispatch: jest.fn()
+    }
+  };
+});
+
+jest.mock('./data/events', () => {
+  return {
+    requestLastWeek: () => 'requestLastWeek'
+  };
+});
+
+it('renders a snapshot', () => {
+  const val = shallow(<App/>);
+  expect(val).toMatchSnapshot();
 });
