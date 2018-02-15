59929c9a99b5f74bd1f3ddb8a3cd9fe727c691d7
John Lenz <wuzzeb@gmail.com>
2018-02-15 13:42:40 -0600

    New swagger and api

--- client/insight/src/data/api.ts
+++ client/insight/src/data/api.ts
@@ -596,11 +596,92 @@ export class JobsClient {
         return Promise.resolve<void>(<any>null);
     }
 
-    setMaterialInQueue(materialID: number, queue: string): Promise<void> {
-        let url_ = this.baseUrl + "/api/v1/jobs/material/{materialID}/queue";
-        if (materialID === undefined || materialID === null)
-            throw new Error("The parameter 'materialID' must be defined.");
-        url_ = url_.replace("{materialID}", encodeURIComponent("" + materialID));
+    queueNames(): Promise<string[]> {
+        let url_ = this.baseUrl + "/queues";
+        url_ = url_.replace(/[?&]$/, "");
+
+        let options_ = <RequestInit>{
+            method: "GET",
+            headers: new Headers({
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            })
+        };
+
+        return this.http.fetch(url_, options_).then((_response: Response) => {
+            return this.processQueueNames(_response);
+        });
+    }
+
+    protected processQueueNames(response: Response): Promise<string[]> {
+        const status = response.status;
+        let _headers: any = {}; if (response.headers && response.headers.forEach) { response.headers.forEach((v, k) => _headers[k] = v); };
+        if (status === 200) {
+            return response.text().then((_responseText) => {
+            let result200: any = null;
+            let resultData200 = _responseText === "" ? null : JSON.parse(_responseText, this.jsonParseReviver);
+            if (resultData200 && resultData200.constructor === Array) {
+                result200 = [];
+                for (let item of resultData200)
+                    result200.push(item);
+            }
+            return result200;
+            });
+        } else if (status !== 200 && status !== 204) {
+            return response.text().then((_responseText) => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            });
+        }
+        return Promise.resolve<string[]>(<any>null);
+    }
+
+    serialsInQueue(queue: string): Promise<string[]> {
+        let url_ = this.baseUrl + "/api/v1/jobs/queue/{queue}/serials";
+        if (queue === undefined || queue === null)
+            throw new Error("The parameter 'queue' must be defined.");
+        url_ = url_.replace("{queue}", encodeURIComponent("" + queue));
+        url_ = url_.replace(/[?&]$/, "");
+
+        let options_ = <RequestInit>{
+            method: "GET",
+            headers: new Headers({
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            })
+        };
+
+        return this.http.fetch(url_, options_).then((_response: Response) => {
+            return this.processSerialsInQueue(_response);
+        });
+    }
+
+    protected processSerialsInQueue(response: Response): Promise<string[]> {
+        const status = response.status;
+        let _headers: any = {}; if (response.headers && response.headers.forEach) { response.headers.forEach((v, k) => _headers[k] = v); };
+        if (status === 200) {
+            return response.text().then((_responseText) => {
+            let result200: any = null;
+            let resultData200 = _responseText === "" ? null : JSON.parse(_responseText, this.jsonParseReviver);
+            if (resultData200 && resultData200.constructor === Array) {
+                result200 = [];
+                for (let item of resultData200)
+                    result200.push(item);
+            }
+            return result200;
+            });
+        } else if (status !== 200 && status !== 204) {
+            return response.text().then((_responseText) => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            });
+        }
+        return Promise.resolve<string[]>(<any>null);
+    }
+
+    setMaterialInQueue(serial: string, queue: string): Promise<void> {
+        let url_ = this.baseUrl + "/api/v1/jobs/material/{serial}/queue";
+        if (serial === undefined || serial === null)
+            throw new Error("The parameter 'serial' must be defined.");
+        url_ = url_.replace("{serial}", encodeURIComponent("" + serial));
         url_ = url_.replace(/[?&]$/, "");
 
         const content_ = JSON.stringify(queue);
@@ -633,11 +714,11 @@ export class JobsClient {
         return Promise.resolve<void>(<any>null);
     }
 
-    removeMaterialFromQueue(materialID: number): Promise<void> {
-        let url_ = this.baseUrl + "/api/v1/jobs/material/{materialID}/queue";
-        if (materialID === undefined || materialID === null)
-            throw new Error("The parameter 'materialID' must be defined.");
-        url_ = url_.replace("{materialID}", encodeURIComponent("" + materialID));
+    removeMaterialFromQueue(serial: string): Promise<void> {
+        let url_ = this.baseUrl + "/api/v1/jobs/material/{serial}/queue";
+        if (serial === undefined || serial === null)
+            throw new Error("The parameter 'serial' must be defined.");
+        url_ = url_.replace("{serial}", encodeURIComponent("" + serial));
         url_ = url_.replace(/[?&]$/, "");
 
         let options_ = <RequestInit>{
@@ -2782,8 +2863,8 @@ export enum ActionType {
 }
 
 export class QueueSize extends ValueType implements IQueueSize {
-    maxSizeBeforeStopLoading: number;
-    maxSizeBeforeStopUnloading: number;
+    maxSizeBeforeStopLoading?: number;
+    maxSizeBeforeStopUnloading?: number;
 
     constructor(data?: IQueueSize) {
         super(data);
@@ -2814,8 +2895,8 @@ export class QueueSize extends ValueType implements IQueueSize {
 }
 
 export interface IQueueSize extends IValueType {
-    maxSizeBeforeStopLoading: number;
-    maxSizeBeforeStopUnloading: number;
+    maxSizeBeforeStopLoading?: number;
+    maxSizeBeforeStopUnloading?: number;
 }
 
 export class NewJobs extends ValueType implements INewJobs {
