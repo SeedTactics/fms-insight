a04d5c787cbd918731ec2cbe12a966450f103802
John Lenz <wuzzeb@gmail.com>
2018-04-19 15:24:33 -0500

    Update version to remove newline

--- build/version.py
+++ build/version.py
@@ -6,12 +6,12 @@ if len(sys.argv) < 2:
     print("Must specify tag prefix")
     sys.exit(1)
 
-curtag = subprocess.check_output(["hg", "id", "-t", "-r", ".^"]).decode("utf-8")
+curtag = subprocess.check_output(["hg", "id", "-t", "-r", ".^"]).decode("utf-8").rstrip()
 prefix = sys.argv[1]
 
 if curtag.startswith(prefix):
     print(curtag.replace(prefix + "-", ""))
 else:
-    tag = subprocess.check_output(["hg", "id", "-t", "-r", "ancestors(.) and tag('re:" + prefix + "')"]).decode("utf-8")
+    tag = subprocess.check_output(["hg", "id", "-t", "-r", "ancestors(.) and tag('re:" + prefix + "')"]).decode("utf-8").rstrip()
     parts = tag.replace(prefix + "-", "").split(".")
     print(parts[0] + "." + parts[1] + "." + str(int(parts[2]) + 1))
