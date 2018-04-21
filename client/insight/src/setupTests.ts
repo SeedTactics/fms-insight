9ed2c0ee562c7759eeb87d9dff27d19cd3f9f64d
John Lenz <wuzzeb@gmail.com>
2018-04-21 15:56:47 -0500

    set timezone in javascript for testing

--- client/insight/src/setupTests.ts
+++ client/insight/src/setupTests.ts
@@ -1,3 +1,10 @@
 import * as Enzyme from 'enzyme';
 import * as Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({ adapter: new Adapter() });
+
+declare var process: {
+   env: {
+       TZ: string
+   }
+};
+process.env.TZ = 'America/Chicago';
