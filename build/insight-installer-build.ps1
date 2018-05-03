fdc2b8212ab228e3aa54f68057f6bef6d86772cd
John Lenz <wuzzeb@gmail.com>
2018-05-03 15:54:30 -0500

    attempt a fix publishing installers

--- build/insight-installer-build.ps1
+++ build/insight-installer-build.ps1
@@ -44,14 +44,16 @@ Move-Item "$publishdir/BlackMaple.FMSInsight.$nameUpper.exe" tmp -Force
 Remove-Item -r tmp
 
 if ($tag.StartsWith($name)) {
+    Push-Location
     cd installers
     Remove-Item alias:curl
-    curl -X POST `
+    curl.exe -X POST `
         --silent --show-error `
         --user $ENV:bitbucket_auth `
-        "https://api.bitbucket.org/2.0/repositories/blackmaple/machinewatch/downloads" `
+        "https://api.bitbucket.org/2.0/repositories/blackmaple/fms-insight/downloads" `
         --form `
         files=@"FMS Insight $nameUpper Install.msi"
+    Pop-Location
 }
 
 Pop-Location
