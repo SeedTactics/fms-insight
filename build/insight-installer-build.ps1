fd0cd5cb08c230a84ca4cf168dd504be3a1ef0e3
John Lenz <wuzzeb@gmail.com>
2018-05-03 13:47:58 -0500

    add and build cincron installer

--- build/insight-installer-build.ps1
+++ build/insight-installer-build.ps1
@@ -5,7 +5,7 @@ $nameUpper = (Get-Culture).TextInfo.ToTitleCase($name)
 
 function ver ($x) { c:\python36\python.exe build/version.py $x }
 $tag = $(hg id -t -r '.^')
-if ($tag.StartsWith("mazak")) {
+if ($tag.StartsWith($name)) {
     $version = $(ver $name)
 } else {
     # WiX doesn't support semver, so don't increment
@@ -29,21 +29,29 @@ $publishdir = "server/machines/$name/bin/Release/net461/publish"
 $ENV:InsightPublishDir = $publishdir
 $ENV:InsightProductId = (New-Guid).Guid
 $ENV:InsightVersion = $version
-$clientdir = "client/insight/build"
-$ENV:InsightClientDir = $clientdir
 
 # Move exe out so it is not captured by heat
 Move-Item "$publishdir/BlackMaple.FMSInsight.$nameUpper.exe" tmp -Force
 
-# Heat for simlab and client
+# Heat for server
 & $heat dir $publishdir -gg -out tmp/insight-server.wsx -sfrag -sreg -srd -var env.InsightPublishDir -dr INSTALLDIR -cg InsightServerCg
-& $heat dir $clientdir  -gg -out tmp/insight-client.wsx -sfrag -sreg -srd -var env.InsightClientDir -dr clientwww -cg InsightClientCg
 
 & $candle tmp/insight-server.wsx -o tmp/insight-server.wixobj
-& $candle tmp/insight-client.wsx -o tmp/insight-client.wixobj
 & $candle build/$name.wsx -o tmp/insight.wixobj -ext WixUtilExtension
-& $light tmp/insight-server.wixobj tmp/insight-client.wixobj tmp/insight.wixobj -ext WixUtilExtension `
+& $light tmp/insight-server.wixobj tmp/insight.wixobj -ext WixUtilExtension `
   -o "installers/FMS Insight $nameUpper Install.msi"
 
 Remove-Item -r tmp
+
+if ($tag.StartsWith($name)) {
+    cd installers
+    Remove-Item alias:curl
+    curl -X POST `
+        --silent --show-error `
+        --user $ENV:bitbucket_auth `
+        "https://api.bitbucket.org/2.0/repositories/blackmaple/machinewatch/downloads" `
+        --form `
+        files=@"FMS Insight $nameUpper Install.msi"
+}
+
 Pop-Location
