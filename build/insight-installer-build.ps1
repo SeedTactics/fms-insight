5c54c28295658acb20cbba03b41f048cd535ac94
John Lenz <wuzzeb@gmail.com>
2018-05-30 16:01:17 -0500

    force create directory for software updates, and include them as artifacts

--- build/insight-installer-build.ps1
+++ build/insight-installer-build.ps1
@@ -42,6 +42,7 @@ Move-Item "$publishdir/BlackMaple.FMSInsight.$nameUpper.exe" tmp -Force
 & $light tmp/insight-server.wixobj tmp/insight.wixobj -ext WixUtilExtension `
   -o "installers/fms-insight/FMS Insight $nameUpper Install.msi"
 
+New-Item -ItemType Directory -Force -Path "software-updates/fms-insight"
 @{"fms-insight" = $name; "version" = $version; "date" = [DateTime]::UtcNow.ToString("u") } `
   | ConvertTo-Json | Out-File -Force $("software-updates/fms-insight/" + $name + "-latest.json")
 
