a5d58347bfded47cccf0e453163bbe73c6605447
John Lenz <wuzzeb@gmail.com>
2018-08-16 17:31:31 -0500

    add basic app shell test

---
+++ client/insight/src/__tests__/app-shell.tsx
@@ -0,0 +1,88 @@
+/* Copyright (c) 2018, John Lenz
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+
+    * Neither the name of John Lenz, Black Maple Software, SeedTactics,
+      nor the names of other contributors may be used to endorse or
+      promote products derived from this software without specific
+      prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import * as React from 'react';
+import { render, cleanup } from 'react-testing-library';
+afterEach(cleanup);
+import { Provider } from 'react-redux';
+import 'jest-dom/extend-expect';
+
+function mockComponent(name: string): (props: {[key: string]: object}) => JSX.Element {
+  return props => (
+    <div data-testid={"mock-component-" + name}>
+      {Object.getOwnPropertyNames(props).sort().map(p =>
+        <span data-prop={p}>
+          {JSON.stringify(props[p], null, 2)}
+        </span>
+      )}
+    </div>
+  );
+}
+
+jest.mock("../components/cost-per-piece/CostPerPiece", () => ({
+  default: mockComponent("CostPerPiece")
+}));
+jest.mock("../components/dashboard/Dashboard", () => ({
+  default: mockComponent("Dashboard")
+}));
+jest.mock("../components/efficiency/Efficiency", () => ({
+  default: mockComponent("Efficiency")
+}));
+jest.mock("../components/station-monitor/StationMonitor", () => ({
+  default: mockComponent("StationMonitor")
+}));
+jest.mock("../components/data-export/DataExport", () => ({
+  default: mockComponent("DataExport")
+}));
+
+(window as any).FMS_INSIGHT_DEMO_MODE = true;
+import { initStore } from '../store/store';
+import App from '../components/App';
+
+it("renders the app shell", () => {
+  const store = initStore();
+
+  const result = render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+
+  expect(
+    result.getByTestId("mock-component-Dashboard")
+  ).toBeDefined();
+  expect(
+    result.queryByTestId("mock-component-StationMonitor")
+  ).not.toBeInTheDocument();
+});
+
