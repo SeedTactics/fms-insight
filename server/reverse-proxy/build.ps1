19684987bd1d459c4faedb0d136181aab3849d28
John Lenz <wuzzeb@gmail.com>
2019-12-13 15:22:28 -0600

    iniital stab at reverse proxy build

---
+++ server/reverse-proxy/build.ps1
@@ -0,0 +1,16 @@
+Param( [string]$version)
+Write-Host "Building FMS Insight reverse proxy version $version"
+
+Push-Location
+Set-Location (Split-Path -parent $PSCommandPath)
+
+if (Test-Path reverse-proxy.zip) {
+  Remove-Item reverse-proxy.zip
+}
+
+dotnet publish -r win10 --self-contained -c Release /p:Version=$version /p:PublishSingleFile=true
+
+& 7z a reverse-proxy.zip * -xr!bin -xr!obj -xr!build-output
+& 7z a reverse-proxy.zip bin/release/netcoreapp3.1/win10/publish/reverse-proxy.exe
+
+Pop-Location
