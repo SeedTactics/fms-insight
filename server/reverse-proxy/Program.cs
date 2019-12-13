0f384afdb57266c9e157a37166d514a22e406eeb
John Lenz <wuzzeb@gmail.com>
2019-12-13 14:57:43 -0600

    add auth to reverse proxy

--- server/reverse-proxy/Program.cs
+++ server/reverse-proxy/Program.cs
@@ -8,31 +8,26 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.AspNetCore.Authentication;
 
 namespace BlackMaple.FMSInsight.ReverseProxy
 {
   public class Program
   {
-    public const string InsightHost = "ip.address.of.insight";
-    public const int InsightPort = 5000;
-    public const int ListenPort = 5000;
-    public const string TLSCertificateFile = null;
-
     public static void Main(string[] args)
     {
-      new WebHostBuilder()
-        .UseKestrel(options =>
+      Host.CreateDefaultBuilder(args)
+        .ConfigureServices((context, services) =>
         {
-          options.Listen(System.Net.IPAddress.IPv6Any, ListenPort, listenOptions =>
-            {
-              if (!string.IsNullOrEmpty(TLSCertificateFile))
-              {
-                listenOptions.UseHttps(TLSCertificateFile);
-              }
-            }
-          );
+          services.Configure<KestrelServerOptions>(context.Configuration.GetSection("Kestrel"));
         })
-        .UseStartup<Startup>()
+        .ConfigureWebHostDefaults(webBuilder => webBuilder
+          .UseStartup<Startup>()
+        )
         .ConfigureLogging(logging => logging.AddConsole())
         .Build()
         .Run();
@@ -40,6 +35,23 @@ namespace BlackMaple.FMSInsight.ReverseProxy
   }
   public class Startup
   {
+    public class InsightProxyConfig
+    {
+      public string NiigataInsightHost { get; set; }
+      public int NiigataInsightPort { get; set; }
+      public string OpenIDConnectAuthority { get; set; }
+      public List<string> AuthTokenAudiences { get; set; }
+    }
+
+    public IConfiguration Configuration { get; }
+    public InsightProxyConfig ProxyConfig { get; }
+    public Startup(IConfiguration cfg)
+    {
+      Configuration = cfg;
+      ProxyConfig = new InsightProxyConfig();
+      Configuration.GetSection("InsightProxy").Bind(ProxyConfig);
+    }
+
     public void ConfigureServices(IServiceCollection services)
     {
       services
@@ -47,6 +59,34 @@ namespace BlackMaple.FMSInsight.ReverseProxy
         .AddCors()
         .AddHttpClient()
         ;
+
+      services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+      .AddJwtBearer(options =>
+      {
+        options.Authority = ProxyConfig.OpenIDConnectAuthority;
+        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+        {
+          ValidateIssuer = true,
+          ValidateAudience = true,
+          ValidateLifetime = true,
+          ValidAudiences = ProxyConfig.AuthTokenAudiences
+        };
+#if DEBUG
+        options.RequireHttpsMetadata = false;
+#endif
+        options.Events = new JwtBearerEvents
+        {
+          OnMessageReceived = context =>
+          {
+            var token = context.Request.Query["token"];
+            if (context.Request.Path == "/api/v1/events" && !string.IsNullOrEmpty(token))
+            {
+              context.Token = token;
+            }
+            return System.Threading.Tasks.Task.CompletedTask;
+          }
+        };
+      });
     }
 
     private readonly HashSet<string> ValidResponseHeaders = new HashSet<string>(new[] {
@@ -58,96 +98,125 @@ namespace BlackMaple.FMSInsight.ReverseProxy
     public void Configure(IApplicationBuilder app, IHttpClientFactory httpFactory, ILogger<Startup> logger)
     {
       app.UseResponseCompression();
-      if (!string.IsNullOrEmpty(Program.TLSCertificateFile))
-      {
-        app.UseHttpsRedirection();
-        app.UseHsts();
-      }
+      app.UseHttpsRedirection();
+      app.UseHsts();
       app.UseCors();
       app.UseWebSockets();
 
-      app.Run(async context =>
+      // authentication
+      app.Use(async (context, next) =>
       {
-        if (context.Request.Path == "/api/v1/events")
+        // the fms-information path is unauthorized, everything else requires auth
+        if (context.Request.Path == "/api/v1/fms/fms-information")
         {
-          if (context.WebSockets.IsWebSocketRequest)
+          await next.Invoke();
+        }
+        else if (context.Request.Path.StartsWithSegments("/api"))
+        {
+          var authResponse = await context.AuthenticateAsync();
+          if (!authResponse.Succeeded)
           {
-            var ws = await context.WebSockets.AcceptWebSocketAsync();
-
-            var buffer = new byte[1024 * 1024];
-            using (var client = new ClientWebSocket())
-            {
-              var uriB = new UriBuilder()
-              {
-                Scheme = "ws",
-                Host = Program.InsightHost,
-                Port = Program.InsightPort,
-                Path = "/api/v1/events"
-              };
-              try
-              {
-                await client.ConnectAsync(uriB.Uri, context.RequestAborted);
-                WebSocketReceiveResult res;
-                do
-                {
-                  res = await client.ReceiveAsync(buffer, context.RequestAborted);
-                  await ws.SendAsync(new ArraySegment<byte>(buffer, 0, res.Count), res.MessageType, res.EndOfMessage, context.RequestAborted);
-                } while (res.MessageType != WebSocketMessageType.Close);
-              }
-              catch (WebSocketException ex) when (ex.WebSocketErrorCode == WebSocketError.ConnectionClosedPrematurely)
-              {
-                //do nothing
-              }
-              catch (Exception ex)
-              {
-                logger.LogInformation(ex, "Error during websocket");
-              }
-              finally
-              {
-                await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, "Server is closing", context.RequestAborted);
-              }
-            }
+            context.Response.StatusCode = 401;
+            return;
           }
           else
           {
-            context.Response.StatusCode = 400;
+            await next.Invoke();
           }
         }
         else
         {
+          // loading static pages
+          await next.Invoke();
+        }
+      });
+
+      // websocket proxy
+      app.Use(async (context, next) =>
+      {
+        if (context.Request.Path != "/api/v1/events")
+        {
+          await next.Invoke();
+          return;
+        }
+        if (!context.WebSockets.IsWebSocketRequest)
+        {
+          context.Response.StatusCode = 400;
+          return;
+        }
+
+        var ws = await context.WebSockets.AcceptWebSocketAsync();
+
+        var buffer = new byte[1024 * 1024];
+        using (var client = new ClientWebSocket())
+        {
           var uriB = new UriBuilder()
           {
-            Scheme = "http",
-            Host = Program.InsightHost,
-            Port = Program.InsightPort,
-            Path = context.Request.Path,
-            Query = context.Request.QueryString.ToUriComponent()
+            Scheme = "ws",
+            Host = ProxyConfig.NiigataInsightHost,
+            Port = ProxyConfig.NiigataInsightPort,
+            Path = "/api/v1/events"
           };
-          var msg = new HttpRequestMessage();
-          msg.RequestUri = uriB.Uri;
-          msg.Method = new HttpMethod(context.Request.Method);
-          if (HttpMethods.IsPut(context.Request.Method) || HttpMethods.IsPost(context.Request.Method))
+          try
           {
-            msg.Content = new StreamContent(context.Request.Body);
-          }
-          foreach (var h in context.Request.Headers)
-          {
-            msg.Content?.Headers.TryAddWithoutValidation(h.Key, h.Value.ToArray());
-          }
-          msg.Headers.Host = msg.RequestUri.Host;
-          var client = httpFactory.CreateClient();
-          using (var resp = await client.SendAsync(msg, HttpCompletionOption.ResponseHeadersRead, context.RequestAborted))
-          {
-            context.Response.StatusCode = (int)resp.StatusCode;
-            foreach (var h in resp.Headers.Concat(resp.Content.Headers))
+            await client.ConnectAsync(uriB.Uri, context.RequestAborted);
+            WebSocketReceiveResult res;
+            do
             {
-              if (ValidResponseHeaders.Contains(h.Key.ToLower()))
-              {
-                context.Response.Headers[h.Key] = h.Value.ToArray();
-              }
-            }
-            await resp.Content.CopyToAsync(context.Response.Body);
+              res = await client.ReceiveAsync(buffer, context.RequestAborted);
+              await ws.SendAsync(new ArraySegment<byte>(buffer, 0, res.Count), res.MessageType, res.EndOfMessage, context.RequestAborted);
+            } while (res.MessageType != WebSocketMessageType.Close);
           }
+          catch (WebSocketException ex) when (ex.WebSocketErrorCode == WebSocketError.ConnectionClosedPrematurely)
+          {
+            //do nothing
+          }
+          catch (Exception ex)
+          {
+            logger.LogInformation(ex, "Error during websocket");
+          }
+          finally
+          {
+            await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, "Server is closing", context.RequestAborted);
+          }
+        }
+      });
+
+      //HTTP proxy
+      app.Run(async context =>
+      {
+        var uriB = new UriBuilder()
+        {
+          Scheme = "http",
+          Host = ProxyConfig.NiigataInsightHost,
+          Port = ProxyConfig.NiigataInsightPort,
+          Path = context.Request.Path,
+          Query = context.Request.QueryString.ToUriComponent()
+        };
+        var msg = new HttpRequestMessage();
+        msg.RequestUri = uriB.Uri;
+        msg.Method = new HttpMethod(context.Request.Method);
+        if (HttpMethods.IsPut(context.Request.Method) || HttpMethods.IsPost(context.Request.Method))
+        {
+          msg.Content = new StreamContent(context.Request.Body);
+        }
+        foreach (var h in context.Request.Headers)
+        {
+          msg.Content?.Headers.TryAddWithoutValidation(h.Key, h.Value.ToArray());
+        }
+        msg.Headers.Host = msg.RequestUri.Host;
+        var client = httpFactory.CreateClient();
+        using (var resp = await client.SendAsync(msg, HttpCompletionOption.ResponseHeadersRead, context.RequestAborted))
+        {
+          context.Response.StatusCode = (int)resp.StatusCode;
+          foreach (var h in resp.Headers.Concat(resp.Content.Headers))
+          {
+            if (ValidResponseHeaders.Contains(h.Key.ToLower()))
+            {
+              context.Response.Headers[h.Key] = h.Value.ToArray();
+            }
+          }
+          await resp.Content.CopyToAsync(context.Response.Body);
         }
       });
     }
