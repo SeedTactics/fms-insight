62dcd14a37b15df52b1053d3737abf1203bc41c5
John Lenz <wuzzeb@gmail.com>
2019-12-13 16:45:59 -0600

    fix path for reverse-proxy build

--- server/reverse-proxy/build.ps1
+++ server/reverse-proxy/build.ps1
@@ -10,7 +10,9 @@ if (Test-Path reverse-proxy.zip) {
 
 dotnet publish -r win10-x64 --self-contained -c Release /p:Version=$version /p:PublishSingleFile=true
 
+Get-ChildItem -Recurse bin/Release/netcoreapp3.1
+
 & 7z a reverse-proxy.zip * -xr!bin -xr!obj -xr!build-output
-& 7z a reverse-proxy.zip bin/release/netcoreapp3.1/win10/publish/reverse-proxy.exe
+& 7z a reverse-proxy.zip bin/Release/netcoreapp3.1/win10-x64/publish/reverse-proxy.exe
 
 Pop-Location
