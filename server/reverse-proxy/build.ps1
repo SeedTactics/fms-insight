420fcbe8fd4ed7a91dcccfc25cfb33fe08c1b2d0
John Lenz <wuzzeb@gmail.com>
2019-12-13 16:03:37 -0600

    reverse-proxy attempt fix for release

--- server/reverse-proxy/build.ps1
+++ server/reverse-proxy/build.ps1
@@ -8,7 +8,7 @@ if (Test-Path reverse-proxy.zip) {
   Remove-Item reverse-proxy.zip
 }
 
-dotnet publish -r win10 --self-contained -c Release /p:Version=$version /p:PublishSingleFile=true
+dotnet publish -r win10-x64 --self-contained -c Release /p:Version=$version /p:PublishSingleFile=true
 
 & 7z a reverse-proxy.zip * -xr!bin -xr!obj -xr!build-output
 & 7z a reverse-proxy.zip bin/release/netcoreapp3.1/win10/publish/reverse-proxy.exe
