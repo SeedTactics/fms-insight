bfed6a7dfc8f22a107c7e266bc295319dfd7a8b7
John Lenz <wuzzeb@gmail.com>
2020-07-16 13:21:22 -0500

    proxy: switch to service

--- server/reverse-proxy/build.ps1
+++ server/reverse-proxy/build.ps1
@@ -4,15 +4,23 @@ Write-Host "Building FMS Insight reverse proxy version $version"
 Push-Location
 Set-Location (Split-Path -parent $PSCommandPath)
 
-if (Test-Path reverse-proxy.zip) {
-  Remove-Item reverse-proxy.zip
+If (Test-Path build) {
+  Remove-Item -r build
 }
+New-Item -ItemType Directory -Force -Path build
 
 dotnet publish -r win10-x64 --self-contained -c Release /p:Version=$version /p:PublishSingleFile=true
 
-Get-ChildItem -Recurse bin/Release/netcoreapp3.1
+Copy-Item bin/Release/netcoreapp3.1/win10-x64/publish/reverse-proxy.exe build/reverse-proxy.exe
 
-& 7z a reverse-proxy.zip * -xr!bin -xr!obj -xr!build-output
-& 7z a reverse-proxy.zip ./bin/Release/netcoreapp3.1/win10-x64/publish/reverse-proxy.exe
+$candle = "C:\Program Files (x86)\WiX Toolset v3.11\bin\candle.exe"
+$light = "C:\Program Files (x86)\WiX Toolset v3.11\bin\light.exe"
+
+$ENV:ProxyProductId = (New-Guid).Guid
+$ENV:ProxyVersion = $version
+
+& $candle reverse-proxy.wsx -o build/reverse-proxy.wixobj -ext WixUtilExtension
+& $light build/reverse-proxy.wixobj -ext WixUtilExtension `
+  -o "build/FMS Insight Reverse Proxy Install.msi"
 
 Pop-Location
