d615d05cbd6e1cb6b7723d1a7cb5a1b5b7c77931
John Lenz <wuzzeb@gmail.com>
2020-08-03 14:37:49 -0500

    server: record if a tool change occurred during the cycle

--- server/lib/BlackMaple.MachineFramework/api/LogData.cs
+++ server/lib/BlackMaple.MachineFramework/api/LogData.cs
@@ -253,6 +253,7 @@ namespace BlackMaple.MachineWatchInterface
     [DataMember(IsRequired = true)] public TimeSpan ToolUseDuringCycle { get; set; }
     [DataMember(IsRequired = true)] public TimeSpan TotalToolUseAtEndOfCycle { get; set; }
     [DataMember(IsRequired = false, EmitDefaultValue = false)] public TimeSpan ConfiguredToolLife { get; set; }
+    [DataMember(IsRequired = false, EmitDefaultValue = false)] public bool? ToolChangeOccurred { get; set; }
   }
 
   // stored serialized in json format in the details for inspection logs.
