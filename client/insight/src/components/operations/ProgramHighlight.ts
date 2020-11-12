24aa4caa7aaade64b36d1de118295735823bb5e3
John Lenz <wuzzeb@gmail.com>
2020-11-12 16:42:18 -0600

    client: fix typescript build

--- client/insight/src/components/operations/ProgramHighlight.ts
+++ client/insight/src/components/operations/ProgramHighlight.ts
@@ -2,6 +2,6 @@ import hljs from "highlight.js/lib/core";
 import gcode from "highlight.js/lib/languages/gcode";
 hljs.registerLanguage("gcode", gcode);
 
-self.onmessage = function (event: MessageEvent<string>) {
+self.onmessage = function (event: MessageEvent) {
   (self.postMessage as any)(hljs.highlight("gcode", event.data).value);
 };
