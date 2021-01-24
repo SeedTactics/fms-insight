84dd788b9dafc2db4f89bb36e657315c313a089b
John Lenz <wuzzeb@gmail.com>
2021-01-24 12:50:40 -0600

    client: enter total labor cost for last 30 days

--- client/insight/src/components/analysis/CostPerPiece.tsx
+++ client/insight/src/components/analysis/CostPerPiece.tsx
@@ -67,12 +67,6 @@ import IconButton from "@material-ui/core/IconButton";
 import { PartIdenticon } from "../station-monitor/Material";
 import Typography from "@material-ui/core/Typography";
 
-interface Last30LaborCost {
-  readonly numOperators: number | null;
-  readonly averageWagePerHour: number | null;
-  readonly hoursPerDay: number;
-}
-
 async function loadMachineCostPerYear(): Promise<MachineCostPerYear> {
   return (await localForage.getItem("MachineCostPerYear")) ?? {};
 }
@@ -89,14 +83,12 @@ async function saveAutomationCostPerYear(n: number | null): Promise<void> {
   await localForage.setItem("AutomationCostPerYear", n);
 }
 
-async function loadLast30LaborCost(): Promise<Last30LaborCost> {
-  return (
-    (await localForage.getItem("Last30LaborCost")) ?? { numOperators: null, averageWagePerHour: null, hoursPerDay: 24 }
-  );
+async function loadLast30LaborCost(): Promise<number | null> {
+  return await localForage.getItem("Last30TotalLaborCost");
 }
 
-async function saveLast30LaborCost(c: Last30LaborCost): Promise<void> {
-  await localForage.setItem("Last30LaborCost", c);
+async function saveLast30LaborCost(c: number | null): Promise<void> {
+  await localForage.setItem("Last30TotalLaborCost", c);
 }
 
 async function loadPerMonthLabor(month: Date): Promise<number | null> {
@@ -107,75 +99,6 @@ async function loadPerMonthLabor(month: Date): Promise<number | null> {
 async function savePerMonthLabor(month: Date, cost: number | null): Promise<void> {
   const key = "PerMonthLabor-" + month.getFullYear().toString() + "-" + month.getMonth().toString();
   await localForage.setItem(key, cost);
-}
-
-interface Last30LaborCostProps {
-  readonly input: Last30LaborCost;
-  readonly setLast30: (l: Last30LaborCost) => void;
-}
-
-function Last30LaborCostInput(props: Last30LaborCostProps) {
-  const [numOper, setNumOper] = React.useState<number | null>(null);
-  const [perHour, setPerHour] = React.useState<number | null>(null);
-  const [hoursPerDay, setHoursPerDay] = React.useState<number | null>(null);
-
-  return (
-    <>
-      <TextField
-        id="num-operators"
-        type="number"
-        label="Number of Operators"
-        inputProps={{ min: 0 }}
-        variant="outlined"
-        value={numOper ? (isNaN(numOper) ? "" : numOper) : props.input.numOperators ?? ""}
-        onChange={(e) => setNumOper(parseInt(e.target.value))}
-        onBlur={() => {
-          if (numOper != null) {
-            const newCost = { ...props.input, numOperators: isNaN(numOper) ? null : numOper };
-            saveLast30LaborCost(newCost);
-            props.setLast30(newCost);
-          }
-          setNumOper(null);
-        }}
-      />
-      <TextField
-        id="cost-per-oper-hour"
-        type="number"
-        style={{ marginTop: "1.5em" }}
-        label="Cost per operator per hour"
-        inputProps={{ min: 0 }}
-        variant="outlined"
-        value={perHour ? (isNaN(perHour) ? "" : perHour) : props.input.averageWagePerHour ?? ""}
-        onChange={(e) => setPerHour(parseFloat(e.target.value))}
-        onBlur={() => {
-          if (perHour != null) {
-            const newCost = { ...props.input, averageWagePerHour: isNaN(perHour) ? null : perHour };
-            saveLast30LaborCost(newCost);
-            props.setLast30(newCost);
-          }
-          setPerHour(null);
-        }}
-      />
-      <TextField
-        id="hours-per-day"
-        type="number"
-        label="Hours Per Day"
-        style={{ marginTop: "1.5em" }}
-        inputProps={{ min: 0, max: 24 }}
-        variant="outlined"
-        value={hoursPerDay ? (isNaN(hoursPerDay) ? "" : hoursPerDay) : props.input.hoursPerDay}
-        onChange={(e) => setHoursPerDay(parseFloat(e.target.value))}
-        onBlur={() => {
-          if (hoursPerDay != null && !isNaN(hoursPerDay)) {
-            const newCost = { ...props.input, hoursPerDay: hoursPerDay };
-            saveLast30LaborCost(newCost);
-            props.setLast30(newCost);
-          }
-          setHoursPerDay(null);
-        }}
-      />
-    </>
-  );
 }
 
 interface AutomationCostInputProps {
@@ -208,28 +131,32 @@ function AutomationCostInput(props: AutomationCostInputProps) {
   );
 }
 
-interface SpecificMonthLaborCostProps {
-  readonly laborCostForMonth: number | null;
-  readonly month: Date;
-  readonly setLaborCostForMonth: (n: number | null) => void;
+interface LaborCostProps {
+  readonly month: Date | null;
+  readonly laborCost: number | null;
+  readonly setLaborCost: (n: number | null) => void;
 }
 
-function SpecificMonthLaborCost(props: SpecificMonthLaborCostProps) {
+function LaborCost(props: LaborCostProps) {
   const [cost, setCost] = React.useState<number | null>(null);
 
   return (
     <TextField
       type="number"
-      label={"Total labor cost for " + format(props.month, "MMMM yyyy")}
+      label={"Total labor cost for " + (props.month === null ? "last 30 days" : format(props.month, "MMMM yyyy"))}
       inputProps={{ min: 0 }}
       variant="outlined"
-      value={cost === null ? props.laborCostForMonth ?? "" : isNaN(cost) ? "" : cost}
+      value={cost === null ? props.laborCost ?? "" : isNaN(cost) ? "" : cost}
       onChange={(e) => setCost(parseFloat(e.target.value))}
       onBlur={() => {
         if (cost != null) {
           const newCost = isNaN(cost) ? null : cost;
-          savePerMonthLabor(props.month, newCost);
-          props.setLaborCostForMonth(newCost);
+          if (props.month === null) {
+            saveLast30LaborCost(newCost);
+          } else {
+            savePerMonthLabor(props.month, newCost);
+          }
+          props.setLaborCost(newCost);
         }
         setCost(null);
       }}
@@ -544,11 +471,7 @@ interface CostPerPieceProps {
 function CostPerPiecePage(props: CostPerPieceProps) {
   const [loading, setLoading] = React.useState(false);
   const [machineCostPerYear, setMachineCostPerYear] = React.useState<MachineCostPerYear>({});
-  const [last30LaborCost, setLast30LaborCost] = React.useState<Last30LaborCost>({
-    numOperators: null,
-    averageWagePerHour: null,
-    hoursPerDay: 24,
-  });
+  const [last30LaborCost, setLast30LaborCost] = React.useState<number | null>(null);
   const [curMonthLaborCost, setCurMonthLaborCost] = React.useState<number | null | "LOADING">(null);
   const [automationCostPerYear, setAutomationCostPerYear] = React.useState<number | null>(null);
 
@@ -590,8 +513,7 @@ function CostPerPiecePage(props: CostPerPieceProps) {
     if (props.month) {
       totalLaborCost = curMonthLaborCost !== "LOADING" ? curMonthLaborCost ?? 0 : 0;
     } else {
-      totalLaborCost =
-        (last30LaborCost.averageWagePerHour ?? 0) * last30LaborCost.hoursPerDay * (last30LaborCost.numOperators ?? 0);
+      totalLaborCost = last30LaborCost ?? 0;
     }
     return compute_monthly_cost(machineCostPerYear, automationCostPerYear, totalLaborCost, props.cycles, props.month);
   }, [
@@ -619,13 +541,9 @@ function CostPerPiecePage(props: CostPerPieceProps) {
         <CostInputCard>
           <div style={{ display: "flex", flexDirection: "column" }}>
             {props.month === null ? (
-              <Last30LaborCostInput input={last30LaborCost} setLast30={setLast30LaborCost} />
+              <LaborCost month={null} laborCost={last30LaborCost} setLaborCost={setLast30LaborCost} />
             ) : (
-              <SpecificMonthLaborCost
-                month={props.month}
-                laborCostForMonth={curMonthLaborCost}
-                setLaborCostForMonth={setCurMonthLaborCost}
-              />
+              <LaborCost month={props.month} laborCost={curMonthLaborCost} setLaborCost={setCurMonthLaborCost} />
             )}
             <AutomationCostInput
               automationCostPerYear={automationCostPerYear}
