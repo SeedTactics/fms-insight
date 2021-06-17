b8cebbff561bd9e1336496666dd08484507a90ed
John Lenz <wuzzeb@gmail.com>
2021-06-17 14:51:43 -0500

    client: fix call to render

--- client/insight/src/index.ts
+++ client/insight/src/index.ts
@@ -50,7 +50,7 @@ if (window.location.pathname === RouteLocation.Backup_InitialOpen) {
   );
 }
 
-render({ demo: false }, store, document.getElementById("root"));
+render(store, document.getElementById("root"));
 
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.getRegistrations().then((registrations) => {
