528f33898c379932be12de60d0e3d455b63d2b53
John Lenz <wuzzeb@gmail.com>
2021-06-17 09:09:30 -0500

    demo data fixes

--- client/insight/src/components/DemoData.tsx
+++ client/insight/src/components/DemoData.tsx
@@ -2,16 +2,31 @@ import * as React from "react";
 import { useRecoilCallback } from "recoil";
 import { JobsBackend } from "../data/backend";
 import { currentStatus } from "../data/current-status";
-import { programReportRefreshTime, toolReportRefreshTime } from "../data/tools-programs";
+import { useRefreshProgramReport, useRefreshToolReport } from "../data/tools-programs";
+import { reduxStore } from "../store/store";
 
 export function LoadDemoData() {
   const load = useRecoilCallback(({ set }) => () => {
     JobsBackend.currentStatus().then((st) => set(currentStatus, st));
-    set(toolReportRefreshTime, new Date());
-    set(programReportRefreshTime, new Date());
   });
+  const loadTools = useRefreshToolReport();
+  const loadPrograms = useRefreshProgramReport();
+  const seenStoreEvents = React.useRef(false);
   React.useEffect(() => {
     load();
+    loadTools();
+    loadPrograms();
+
+    if (reduxStore?.getState().Events.last30.cycles.part_cycles.isEmpty()) {
+      // need to refresh tools and programs once the events load
+      reduxStore?.subscribe(() => {
+        if (!seenStoreEvents.current && !reduxStore?.getState().Events.last30.cycles.part_cycles.isEmpty()) {
+          seenStoreEvents.current = true;
+          loadTools();
+          loadPrograms();
+        }
+      });
+    }
   }, []);
 
   return null;
