6827e3a5c0261af58190ac3feb411e627d124a21
John Lenz <wuzzeb@gmail.com>
2021-06-21 10:24:07 -0500

    inline seedtactic logo

---
+++ client/insight/src/seedtactics-logo.tsx
@@ -0,0 +1,219 @@
+import * as React from "react";
+
+export function SeedtacticLogo(): JSX.Element {
+  return (
+    <svg width="30px" height="30px" viewBox="0 0 170 170" xmlns="http://www.w3.org/2000/svg">
+      <defs>
+        <path
+          id="t"
+          d="m307.5 125v77.5h-40c6.92-6.52 11.25-10.6 12.98-12.23 10.65-10.04 13.56-25.81 7.21-38.99-1.7-3.5-5.92-12.26-12.69-26.28z"
+        />
+        <path
+          id="ag"
+          d="m273.88 120.52c-0.13 0.28-4.87 3.3-11.17 0.5-6.3-2.81-11.99-9.96-11.67-10.67 0.32-0.72 9.44-1.27 15.74 1.53 6.3 2.81 7.23 8.35 7.1 8.64z"
+        />
+        <path
+          id="m"
+          d="m259 102.25c-0.13 0.29-4.2 3.61-9.02 1.46-4.81-2.14-8.75-8.51-8.43-9.23 0.31-0.71 7.69-2.05 12.5 0.1 4.82 2.14 5.07 7.39 4.95 7.67z"
+        />
+        <path
+          id="x"
+          d="m282.79 114.2c-0.2 0.24-5.59 1.84-10.87-2.59-5.29-4.44-8.78-12.88-8.28-13.48 0.5-0.59 9.42 1.38 14.7 5.82 5.29 4.43 4.65 10.02 4.45 10.25z"
+        />
+        <path
+          id="k"
+          d="m268.39 125.43c-0.12-0.28-4.33-3.54-9.45-1.26s-9.41 8.81-9.1 9.52c0.32 0.71 8.05 1.89 13.17-0.39s5.51-7.58 5.38-7.87z"
+        />
+        <path id="ae" d="m243.83 111.58c0 0.31-2.61 5-8.43 5s-12.65-4.22-12.65-5 6.83-5 12.65-5 8.43 4.69 8.43 5z" />
+        <path
+          id="l"
+          d="m304.24 109.88c-0.31 0.04-5.38-2.38-6.34-9.21s2.09-15.44 2.86-15.55 6.08 7.33 7.04 14.16-3.25 10.55-3.56 10.6z"
+        />
+        <path
+          id="u"
+          d="m306.79 80.57c-0.31 0.05-5.31-1.83-6.1-7.47-0.79-5.63 2.45-12.84 3.23-12.95 0.77-0.11 5.88 5.92 6.67 11.56s-3.49 8.82-3.8 8.86z"
+        />
+        <path
+          id="a"
+          d="m306.13 58.04c-0.31 0.05-5.23-1.3-5.86-5.74-0.62-4.43 2.83-10.24 3.6-10.34 0.77-0.11 5.68 4.51 6.31 8.95 0.62 4.44-3.74 7.09-4.05 7.13z"
+        />
+        <path
+          id="f"
+          d="m295.52 118.45c-0.29 0.13-6.1-1.39-9.5-9.02-3.39-7.64-3.53-18.31-2.82-18.63 0.72-0.32 8.56 6.93 11.96 14.56s0.64 12.97 0.36 13.09z"
+        />
+        <path
+          id="b"
+          d="m272.35 90.11c-0.26 0.19-5.76 0.58-9.58-4.67-3.82-5.26-4.88-13.9-4.25-14.36s8.52 3.23 12.34 8.48 1.74 10.37 1.49 10.55z"
+        />
+        <path
+          id="s"
+          d="m289.97 85.66c-0.28 0.13-5.97-0.97-9.17-7.84-3.21-6.87-3.15-16.73-2.44-17.06s8.3 5.96 11.5 12.83 0.4 11.94 0.11 12.07z"
+        />
+        <path
+          id="n"
+          d="m270.16 65.57c-0.28 0.14-5.38 0.38-7.35-3.65-1.96-4.03-0.48-10.62 0.22-10.96s6.8 2.54 8.77 6.58c1.97 4.03-1.36 7.9-1.64 8.03z"
+        />
+        <path
+          id="ah"
+          d="m250 80c-0.17 0.26-4.4 3.11-8.12 0.69s-5.79-8.79-5.37-9.45c0.43-0.65 7.1-1.35 10.82 1.06 3.72 2.42 2.84 7.44 2.67 7.7z"
+        />
+        <path
+          id="y"
+          d="m236.47 96.58c-0.11 0.29-3.72 3.89-7.83 2.23-4.12-1.66-7.37-7.53-7.07-8.25 0.29-0.72 6.7-2.68 10.81-1.02 4.12 1.66 4.21 6.75 4.09 7.04z"
+        />
+        <path
+          id="aa"
+          d="m290.52 56.48c-0.28 0.14-5.35 0.58-7.44-3.33-2.08-3.92-0.8-10.5-0.11-10.87s6.86 2.25 8.94 6.17c2.09 3.92-1.12 7.88-1.39 8.03z"
+        />
+        <path
+          id="af"
+          d="m274.18 138.46c-0.16-0.26-5.28-2.6-11.13 1.06s-10.48 11.53-10.07 12.19 9.52-0.05 15.37-3.71 6-9.27 5.83-9.54z"
+        />
+        <path
+          id="z"
+          d="m247.89 137.78c0.11 0.29-0.65 5.6-6.08 7.69-5.43 2.08-13.32 0.59-13.6-0.14s4.58-7.11 10.01-9.2c5.44-2.08 9.56 1.36 9.67 1.65z"
+        />
+        <path
+          id="h"
+          d="m235.06 125.39c0.1 0.3-0.26 5.38-4.46 6.83-4.19 1.44-10.49-0.85-10.75-1.59-0.25-0.74 3.3-6.42 7.5-7.87 4.19-1.44 7.61 2.34 7.71 2.63z"
+        />
+        <path
+          id="ab"
+          d="m317.14 85.63c0.28 0.13 5.97-0.98 9.17-7.85 3.21-6.87 3.14-16.72 2.44-17.05-0.71-0.33-8.3 5.95-11.5 12.82-3.21 6.87-0.4 11.94-0.11 12.08z"
+        />
+        <path
+          id="ac"
+          d="m299.61 124.96v77.5h40c-6.93-6.52-11.25-10.6-12.99-12.23-10.64-10.03-13.56-25.81-7.2-38.99 1.69-3.5 5.92-12.26 12.69-26.28z"
+        />
+        <path
+          id="c"
+          d="m348.11 102.22c0.13 0.28 4.2 3.6 9.01 1.46 4.82-2.15 8.76-8.52 8.44-9.23-0.32-0.72-7.69-2.05-12.5 0.09-4.82 2.14-5.08 7.39-4.95 7.68z"
+        />
+        <path
+          id="p"
+          d="m324.32 114.17c0.2 0.24 5.59 1.83 10.87-2.6 5.29-4.43 8.78-12.87 8.28-13.47s-9.42 1.38-14.71 5.81c-5.28 4.43-4.64 10.02-4.44 10.26z"
+        />
+        <path
+          id="o"
+          d="m338.71 125.4c0.13-0.29 4.34-3.55 9.46-1.27s9.41 8.81 9.09 9.52c-0.31 0.72-8.04 1.9-13.16-0.38s-5.51-7.59-5.39-7.87z"
+        />
+        <path
+          id="j"
+          d="m363.28 111.54c0 0.32 2.61 5 8.43 5s12.65-4.21 12.65-5c0-0.78-6.83-5-12.65-5s-8.43 4.69-8.43 5z"
+        />
+        <path
+          id="w"
+          d="m311.59 118.42c0.29 0.12 6.1-1.4 9.49-9.03 3.4-7.63 3.54-18.31 2.82-18.63-0.71-0.32-8.55 6.93-11.95 14.56s-0.64 12.97-0.36 13.1z"
+        />
+        <path
+          id="i"
+          d="m334.76 90.08c0.25 0.18 5.76 0.57 9.58-4.68 3.81-5.25 4.88-13.9 4.25-14.36s-8.53 3.23-12.34 8.48c-3.82 5.26-1.74 10.37-1.49 10.56z"
+        />
+        <path
+          id="r"
+          d="m333.23 120.48c0.12 0.29 4.86 3.31 11.17 0.5 6.3-2.81 11.98-9.96 11.67-10.67-0.32-0.71-9.44-1.27-15.74 1.53-6.3 2.81-7.23 8.35-7.1 8.64z"
+        />
+        <path
+          id="ad"
+          d="m336.95 65.54c0.28 0.13 5.38 0.38 7.35-3.66 1.96-4.03 0.48-10.61-0.22-10.96-0.7-0.34-6.81 2.55-8.77 6.58-1.97 4.03 1.36 7.9 1.64 8.04z"
+        />
+        <path
+          id="ai"
+          d="m357.11 79.96c0.17 0.27 4.39 3.11 8.12 0.69 3.72-2.41 5.79-8.79 5.36-9.44-0.42-0.66-7.09-1.36-10.81 1.06-3.72 2.41-2.84 7.43-2.67 7.69z"
+        />
+        <path
+          id="d"
+          d="m370.63 96.55c0.12 0.29 3.73 3.89 7.84 2.22 4.11-1.66 7.37-7.52 7.07-8.25-0.29-0.72-6.7-2.68-10.82-1.02-4.11 1.66-4.2 6.76-4.09 7.05z"
+        />
+        <path
+          id="q"
+          d="m316.59 56.44c0.28 0.15 5.35 0.59 7.43-3.33 2.09-3.92 0.81-10.5 0.12-10.87-0.69-0.36-6.86 2.26-8.95 6.17-2.08 3.92 1.12 7.88 1.4 8.03z"
+        />
+        <path
+          id="v"
+          d="m332.93 138.42c0.16-0.26 5.28-2.59 11.13 1.06 5.85 3.66 10.48 11.53 10.07 12.19-0.42 0.66-9.52-0.05-15.37-3.71-5.85-3.65-6-9.27-5.83-9.54z"
+        />
+        <path
+          id="g"
+          d="m359.22 137.74c-0.11 0.29 0.65 5.61 6.08 7.69 5.43 2.09 13.32 0.59 13.6-0.13 0.28-0.73-4.59-7.12-10.02-9.21-5.43-2.08-9.55 1.36-9.66 1.65z"
+        />
+        <path
+          id="e"
+          d="m372.05 125.36c-0.1 0.29 0.26 5.37 4.45 6.82 4.2 1.44 10.5-0.85 10.75-1.59 0.26-0.74-3.29-6.42-7.49-7.87-4.2-1.44-7.61 2.34-7.71 2.64z"
+        />
+      </defs>
+      <g transform="translate(-219.32 -33)" visibility="inherit">
+        <g visibility="inherit">
+          <use width="100%" height="100%" fill="#DBA72E" href="#t" />
+          <use width="100%" height="100%" fillOpacity="0" stroke="#DBA72E" href="#t" />
+          <use width="100%" height="100%" fill="#DBA72E" href="#ag" />
+          <use width="100%" height="100%" fillOpacity="0" stroke="#DBA72E" href="#ag" />
+          <use width="100%" height="100%" fill="#DBA72E" href="#m" />
+          <use width="100%" height="100%" fillOpacity="0" stroke="#DBA72E" href="#m" />
+          <use width="100%" height="100%" fill="#DBA72E" href="#x" />
+          <use width="100%" height="100%" fillOpacity="0" stroke="#DBA72E" href="#x" />
+          <use width="100%" height="100%" fill="#DBA72E" href="#k" />
+          <use width="100%" height="100%" fillOpacity="0" stroke="#DBA72E" href="#k" />
+          <use width="100%" height="100%" fill="#DBA72E" href="#ae" />
+          <use width="100%" height="100%" fillOpacity="0" stroke="#DBA72E" href="#ae" />
+          <use width="100%" height="100%" fill="#DBA72E" href="#l" />
+          <use width="100%" height="100%" fillOpacity="0" stroke="#DBA72E" href="#l" />
+          <use width="100%" height="100%" fill="#DBA72E" href="#u" />
+          <use width="100%" height="100%" fillOpacity="0" stroke="#DBA72E" href="#u" />
+          <use width="100%" height="100%" fill="#DBA72E" href="#a" />
+          <use width="100%" height="100%" fillOpacity="0" stroke="#DBA72E" href="#a" />
+          <use width="100%" height="100%" fill="#DBA72E" href="#f" />
+          <use width="100%" height="100%" fillOpacity="0" stroke="#DBA72E" href="#f" />
+          <use width="100%" height="100%" fill="#DBA72E" href="#b" />
+          <use width="100%" height="100%" fillOpacity="0" stroke="#DBA72E" href="#b" />
+          <use width="100%" height="100%" fill="#DBA72E" href="#s" />
+          <use width="100%" height="100%" fillOpacity="0" stroke="#DBA72E" href="#s" />
+          <use width="100%" height="100%" fill="#DBA72E" href="#n" />
+          <use width="100%" height="100%" fillOpacity="0" stroke="#DBA72E" href="#n" />
+          <use width="100%" height="100%" fill="#DBA72E" href="#ah" />
+          <use width="100%" height="100%" fill="#DBA72E" href="#y" />
+          <use width="100%" height="100%" fillOpacity="0" stroke="#DBA72E" href="#y" />
+          <use width="100%" height="100%" fill="#DBA72E" href="#aa" />
+          <use width="100%" height="100%" fillOpacity="0" stroke="#DBA72E" href="#aa" />
+          <use width="100%" height="100%" fill="#DBA72E" href="#af" />
+          <use width="100%" height="100%" fillOpacity="0" stroke="#DBA72E" href="#af" />
+          <use width="100%" height="100%" fill="#DBA72E" href="#z" />
+          <use width="100%" height="100%" fillOpacity="0" stroke="#DBA72E" href="#z" />
+          <use width="100%" height="100%" fill="#DBA72E" href="#h" />
+          <use width="100%" height="100%" fillOpacity="0" stroke="#DBA72E" href="#h" />
+          <use width="100%" height="100%" fill="#DBA72E" href="#ab" />
+          <use width="100%" height="100%" fillOpacity="0" stroke="#DBA72E" href="#ab" />
+          <use width="100%" height="100%" fill="#DBA72E" href="#ac" />
+          <use width="100%" height="100%" fillOpacity="0" stroke="#DBA72E" href="#ac" />
+          <use width="100%" height="100%" fill="#DBA72E" href="#c" />
+          <use width="100%" height="100%" fillOpacity="0" stroke="#DBA72E" href="#c" />
+          <use width="100%" height="100%" fill="#DBA72E" href="#p" />
+          <use width="100%" height="100%" fillOpacity="0" stroke="#DBA72E" href="#p" />
+          <use width="100%" height="100%" fill="#DBA72E" href="#o" />
+          <use width="100%" height="100%" fillOpacity="0" stroke="#DBA72E" href="#o" />
+          <use width="100%" height="100%" fill="#DBA72E" href="#j" />
+          <use width="100%" height="100%" fillOpacity="0" stroke="#DBA72E" href="#j" />
+          <use width="100%" height="100%" fill="#DBA72E" href="#w" />
+          <use width="100%" height="100%" fillOpacity="0" stroke="#DBA72E" href="#w" />
+          <use width="100%" height="100%" fill="#DBA72E" href="#i" />
+          <use width="100%" height="100%" fillOpacity="0" stroke="#DBA72E" href="#i" />
+          <use width="100%" height="100%" fill="#DBA72E" href="#r" />
+          <use width="100%" height="100%" fillOpacity="0" stroke="#DBA72E" href="#r" />
+          <use width="100%" height="100%" fill="#DBA72E" href="#ad" />
+          <use width="100%" height="100%" fillOpacity="0" stroke="#DBA72E" href="#ad" />
+          <use width="100%" height="100%" fill="#DBA72E" href="#ai" />
+          <use width="100%" height="100%" fill="#DBA72E" href="#d" />
+          <use width="100%" height="100%" fillOpacity="0" stroke="#DBA72E" href="#d" />
+          <use width="100%" height="100%" fill="#DBA72E" href="#q" />
+          <use width="100%" height="100%" fillOpacity="0" stroke="#DBA72E" href="#q" />
+          <use width="100%" height="100%" fill="#DBA72E" href="#v" />
+          <use width="100%" height="100%" fillOpacity="0" stroke="#DBA72E" href="#v" />
+          <use width="100%" height="100%" fill="#DBA72E" href="#g" />
+          <use width="100%" height="100%" fillOpacity="0" stroke="#DBA72E" href="#g" />
+          <use width="100%" height="100%" fill="#DBA72E" href="#e" />
+          <use width="100%" height="100%" fillOpacity="0" stroke="#DBA72E" href="#e" />
+        </g>
+      </g>
+    </svg>
+  );
+}
