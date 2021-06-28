22c616568636ac77a6c997548ba0883e9a3de1b7
John Lenz <wuzzeb@gmail.com>
2021-06-28 14:26:24 -0500

    client: add legacy build

--- client/insight/vite.config.js
+++ client/insight/vite.config.js
@@ -2,10 +2,11 @@
  * @type {import('vite').UserConfig}
  */
 import reactRefresh from "@vitejs/plugin-react-refresh";
+import legacy from "@vitejs/plugin-legacy";
 
 export default {
   root: "src",
-  plugins: [reactRefresh()],
+  plugins: [reactRefresh(), legacy({ targets: ["defaults", "not IE 11", "edge 18"] })],
   base: "./",
   server: {
     port: 1234,
@@ -13,5 +14,6 @@ export default {
   build: {
     outDir: "../dist", // relative to root
     chunkSizeWarningLimit: 1800,
+    emptyOutDir: true,
   },
 };
