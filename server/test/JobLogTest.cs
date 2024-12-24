8bc84f1423c27f98a5e8a1d5ed0cc0c43049beba
John Lenz <wuzzeb@gmail.com>
2024-12-24 10:39:34 -0600

    server: lengthen wait for queued material

--- server/test/JobLogTest.cs
+++ server/test/JobLogTest.cs
@@ -5476,7 +5476,7 @@ namespace MachineWatchTest
 
       // The sends to external queues happen on a new thread so need to wait
       int numWaits = 0;
-      while (server.LogEntries.Count() < 1 && numWaits < 10)
+      while (server.LogEntries.Count() < 1 && numWaits < 20)
       {
         await Task.Delay(100);
         numWaits++;
