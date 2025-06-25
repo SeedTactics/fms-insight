f2c45ba5a9a2d9f3144b1fc70965b5271f4b205e
John Lenz <wuzzeb@gmail.com>
2025-06-25 13:31:54 -0500

    client: small updates to python sample loading

--- client/log-dump.py
+++ client/log-dump.py
@@ -8,7 +8,9 @@ import urllib.parse
 
 host = "http://localhost:5000"
 
-# Load the last seen event counter from the file names in the current directory
+# Each event has an int64 counter that is guaranteed to be unique and increasing.
+# Load the last seen event counter from the file names in the current directory.
+# If the processed data is instead in a database, can load the last seen counter from there instead.
 lastSeenCntr = None
 for f in os.listdir("."):
     if os.path.isfile(f):
@@ -18,15 +20,22 @@ for f in os.listdir("."):
 
 if lastSeenCntr:
     # load recent events (if any)
-    with urllib.request.urlopen("%s/api/v1/log/events/recent?lastSeenCounter=%i" % (host, lastSeenCntr)) as r:
+    with urllib.request.urlopen(
+        "%s/api/v1/log/events/recent?lastSeenCounter=%i" % (host, lastSeenCntr)
+    ) as r:
         events = json.loads(r.read())
 else:
-    # Load the last 7 days of events
+    # Load the last 2 days of events
     now = datetime.datetime.utcnow()
-    query = urllib.parse.urlencode({
-      "startUTC": (now - datetime.timedelta(days=7)).strftime("%Y-%m-%dT%H:%M:%SZ"),
-      "endUTC": now.strftime("%Y-%m-%dT%H:%M:%SZ")
-    })
+    start = now - datetime.timedelta(days=2)
+
+    # The start and end UTC times are in ISO 8601 format.
+    query = urllib.parse.urlencode(
+        {
+            "startUTC": start.strftime("%Y-%m-%dT%H:%M:%SZ"),
+            "endUTC": now.strftime("%Y-%m-%dT%H:%M:%SZ"),
+        }
+    )
     req = urllib.request.Request("%s/api/v1/log/events/all?%s" % (host, query))
     req.add_header("Accept", "application/json")
     with urllib.request.urlopen(req) as r:
@@ -35,5 +44,12 @@ else:
 # Write the events as files, one event per file.  The name of the file is the event counter so
 # that the lastSeenCounter can easily be computed from the filenames.
 for event in events:
-    with open(str(event["counter"]) + ".json", "w") as f:
-        json.dump(event, f)
+    # Only look at the machine cycle end events:
+    # The loc and locnum fields contain the specific machine.
+    # The elapsed field contains the wall clock time from machine start to end in ISO 8601 duration format.
+    # The active field contains the target time from the flexplan file in ISO 8601 duration format.
+    # The material field contains the list of material. Each material has a unique int64 id along with
+    #   the serial, partname, process number, and other fields.
+    if event["type"] == "MachineCycle" and not event["startofcycle"]:
+        with open(str(event["counter"]) + ".json", "w") as f:
+            json.dump(event, f)
--- client/log-watch.py
+++ client/log-watch.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import asyncio
 import websockets
@@ -6,17 +6,22 @@ import json
 
 uri = "ws://localhost:5000/api/v1/events"
 
-async def consume():
-  async with websockets.connect(uri) as websocket:
-    async for message in websocket:
-      j = json.loads(message)
-      if "LogEntry" in j:
-        print(json.dumps(j["LogEntry"], indent=2))
-        print("")
-        print("  -------------------------------------   ")
-        print("")
-      elif "NewCurrentStatus" in j:
-        st = j["NewCurrentStatus"]
-        print("New status with %i jobs and %i material" % (len(st["Jobs"]), len(st["Material"])))
 
-asyncio.get_event_loop().run_until_complete(consume())
+async def consume():
+    async with websockets.connect(uri) as websocket:
+        async for message in websocket:
+            j = json.loads(message)
+            if "LogEntry" in j:
+                print(json.dumps(j["LogEntry"], indent=2))
+                print("")
+                print("  -------------------------------------   ")
+                print("")
+            elif "NewCurrentStatus" in j:
+                st = j["NewCurrentStatus"]
+                print(
+                    "New status with %i jobs and %i material"
+                    % (len(st["Jobs"]), len(st["Material"]))
+                )
+
+
+asyncio.run(consume())
